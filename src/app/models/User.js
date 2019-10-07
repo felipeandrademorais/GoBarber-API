87b921fb3832b6c40d2d65d30e03894429663135
FELIPE ANDRADE MORAIS <felipe.m.tecnico@sistemafiesc.org.br>
2019-10-07 14:29:55 -0300

    First Commit

---
+++ src/app/models/User.js
@@ -0,0 +1,37 @@
+import Sequelize, { Model } from 'sequelize';
+import bcry from 'bcryptjs';
+
+class User extends Model {
+    static init(sequelize) {
+        super.init(
+            {
+                name: Sequelize.STRING,
+                email: Sequelize.STRING,
+                password: Sequelize.VIRTUAL,
+                password_hash: Sequelize.STRING,
+                provider: Sequelize.BOOLEAN,
+            },
+            {
+                sequelize,
+            }
+        );
+
+        this.addHook('beforeSave', async user => {
+            if (user.password) {
+                user.password_hash = await bcry.hash(user.password, 8);
+            }
+        });
+
+        return this;
+    }
+
+    static associate(models) {
+        this.belongsTo(models.File, { foreignKey: 'avatar_id', as: 'avatar' });
+    }
+
+    checkPassword(password) {
+        return bcry.compare(password, this.password_hash);
+    }
+}
+
+export default User;
